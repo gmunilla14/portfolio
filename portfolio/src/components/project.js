8fd92a90044b77deb2d090c3d6157b07b8169b43
Greg Munilla <gregmunilla@gmail.com>
2022-01-20 15:25:25 -0500

    Create project section

--- portfolio/src/components/project.js
+++ portfolio/src/components/project.js
@@ -1,5 +1,22 @@
-const Project = () => {
-    return ( <div className="project-holder"></div> );
-}
- 
+import Skill from "./skill";
+
+const Project = ({ title, metadata, oneLiner, description, skills }) => {
+  return (
+    <div className="project-holder">
+      <img className="project-image" src="https://via.placeholder.com/400" />
+      <div className="project-content">
+        <div className="project-title">{title}</div>
+        <div className="project-metadata">{metadata}</div>
+        <div className="project-oneliner">{oneLiner}</div>
+        <div className="project-description">{description}</div>
+        <div className="skills-holder" style={{ justifyContent: "start" }}>
+          {skills.map((skill) => {
+            return <Skill text={skill} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Project;
--- portfolio/src/sections/projects.js
+++ portfolio/src/sections/projects.js
@@ -1,7 +1,25 @@
 import Project from "../components/project";
 
 const Projects = () => {
-  return <Project />;
+  return (
+    <Project
+      title="This is the title of the portfolio article and it wraps"
+      metadata="Web App | Personal Project"
+      oneLiner="A one liner describing what this project is all about."
+      description="A short description with what you this project is all about and why
+  you built it. Shouldn’t be more than a couple of lines."
+      skills={[
+        "HTML",
+        "CSS",
+        "React",
+        "MongoDB",
+        "Node",
+        "Express",
+        "Redux",
+        "Bootstrap",
+      ]}
+    />
+  );
 };
 
 export default Projects;
