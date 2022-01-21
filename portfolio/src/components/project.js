78eeb3877749626a07dcff9ed6d06d87e1afcb4f
Greg Munilla <gregmunilla@gmail.com>
2022-01-20 20:05:36 -0500

    Incorporate preliminary responsiveness

--- portfolio/src/components/project.js
+++ portfolio/src/components/project.js
@@ -3,7 +3,7 @@ import Skill from "./skill";
 const Project = ({ title, metadata, oneLiner, description, skills }) => {
   return (
     <div className="project-holder">
-      <img className="project-image" src="https://via.placeholder.com/400" />
+      <img className="project-image" src="https://via.placeholder.com/400" style={{width: "50%"}}/>
       <div className="project-content">
         <div className="project-title">{title}</div>
         <div className="project-metadata">{metadata}</div>
